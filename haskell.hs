e803eefa412292a6554b83fd8219cd19dfec388d
SaVa111 <sawa_96@bk.ru>
2018-05-07 22:15:07 +0300

    Update haskell.hs

--- haskell.hs
+++ haskell.hs
@@ -19,3 +19,12 @@ insert_list l1 l2 0 = l2 ++ l1
 insert_list (h1:t1) l2 n = h1 : insert_list t1 l2 (n - 1)
 
 main = print $ (insert_list [1, 2, 6, 7] [3, 4, 5] 2)
+
+;25. Определите функцию, удаляющую из списка каждый четный элемент.
+del_evens [] = []
+del_evens (h:t) = 
+    if  (rem h 2) == 0 
+        then del_evens t
+        else h : del_evens t
+    
+main = print $ (del_evens [1, 2, 3, 4])
